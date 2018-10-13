7370990df530a85940f1de4dc32516891c7d7254
Fredrik Sjögren <Fredrik@Fredriks-MacBook-Pro.local>
2018-10-13 17:18:12 +0200

    TJENARE KINGEN HÄR KOMMER ÖLEN

--- src/stuff/beer.java
+++ src/stuff/beer.java
@@ -2,21 +2,48 @@ package stuff;
 
 public class beer {
 
-    public final double BURK_STORLEK = 0.33;
+    public final double BURK_STORLEK = 0.33;  // These are gonna be updated lated on
     public final double EN_STOR_STARK = 0.50;
     public final double ÖL_KANNA = 2.62;
     private String typeOfBeer;
     private double price;
-    private String beertype;
 
-    public beer(String typeOfBeer, double price, String beertype){
+
+    public beer(String typeOfBeer, double price){
         this.typeOfBeer = typeOfBeer;
         this.price = price;
-        this.beertype = beertype;
+
 
     }
 
 
+        // Will add some cool methods later on boys
 
+    public double getBURK_STORLEK() {
+        return BURK_STORLEK;
+    }
 
+    public double getEN_STOR_STARK() {
+        return EN_STOR_STARK;
+    }
+
+    public double getÖL_KANNA() {
+        return ÖL_KANNA;
+    }
+
+    public String getTypeOfBeer() {
+        return typeOfBeer;
+    }
+
+    public void setTypeOfBeer(String typeOfBeer) {
+        this.typeOfBeer = typeOfBeer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
