3c7bb03b0565e0b64c9a969ee4469bbd39daf6a6
unknown <marcus.dag.aronsson@gmail.com>
2018-10-13 14:17:09 +0200

    created shit

---
+++ src/stuff/Main.java
@@ -0,0 +1,14 @@
+package stuff;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int a = 5;
+	    int b = 10;
+	    someShittyMethod(a, b);
+    }
+
+    public static void someShittyMethod(int a, int b){
+        System.out.println(a+b);
+    }
+}
